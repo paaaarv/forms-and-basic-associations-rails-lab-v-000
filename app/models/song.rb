47489d6516ebf962ad4bab348e75379739822900
Parv Kumar <parvathikumar92@gmail.com>
2017-09-28 22:07:32 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,20 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  attr_reader :artist_name, :genre_name
+
+  def artist_name=(name)
+    @artist = Artist.find_or_create_by(name: name)
+    self.artist = @artist
+  end
+
+  def genre_name=(ids)
+    if !ids.empty?
+      ids.each do |id|
+        genre = Genre.find(id)
+        self.genres << genre
+      end
+    end
+  end
+end
